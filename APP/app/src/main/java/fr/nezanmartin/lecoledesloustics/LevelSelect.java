17a5f61b1b88a12924a473df97e4fa88276f957d
the_new_sky <loris@xtns.dev>
2021-03-26 11:31:26 +0100

    Styling

--- APP/app/src/main/java/fr/nezanmartin/lecoledesloustics/LevelSelect.java
+++ APP/app/src/main/java/fr/nezanmartin/lecoledesloustics/LevelSelect.java
@@ -2,11 +2,14 @@ package fr.nezanmartin.lecoledesloustics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -87,10 +90,20 @@ public class LevelSelect extends AppCompatActivity {
                 Button levelButtonButton = levelButton.findViewById(R.id.level_button_button);
                 levelButtonButton.setText(String.valueOf(level.getDifficulty()));
 
+                RatingBar levelButtonScore = levelButton.findViewById(R.id.level_button_score);
+                levelButtonScore.setRating(level.getScore());
+
                 levelActivityArea.addView(levelButton);
             }
 
             levelSelectArea.addView(levelActivity);
         }
+    }
+
+    public void switchUser(View v) {
+        //Create intent
+        Intent main = new Intent(LevelSelect.this, MainActivity.class);
+        main.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(main);
     }
 }
